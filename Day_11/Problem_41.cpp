32a2f895372929e71cd9bdfdf85375fc5566bde6
rudrak2005 <rudrakk2005@gmail.com>
2026-06-07 19:45:23 +0100

    Updated Day 11

--- Day_11/Problem_41.cpp
+++ Day_11/Problem_41.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
  using namespace std;
- int findSum(int a, int b)
- {
-    return a + b;
- }
- int main()
- {
-    int num1, num2;
-    cout << "Enter two numbers: ";
-    cin >> num1 >> num2;
-    int sum = findSum(num1, num2);
-    cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << endl;
-    return 0;
- }
+int isPalindrome(int n){
+   int original = n;
+   int reverse = 0;
+   while(n>0){
+      int digit =n%10;
+      reverse = reverse*10+digit;
+      n/=10;
+   }
+   return original == reverse;
+}
